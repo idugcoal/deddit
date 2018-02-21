357cb6768d5fb73cf74ad1c99d36670e9979977d
Doug Cole <idugcoal@gmail.com>
2018-02-21 11:18:10 -0800

    cleanup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import styles from "./App.css";
+import React, { Component } from 'react';
+import styles from './App.css';
+// import StuffList from '../stuffList';
 
 class App extends Component {
   render() {
-    return <div className={styles.test}>testing 123</div>;
+    return <div className={styles.test}>deddit</div>;
   }
 }
 
