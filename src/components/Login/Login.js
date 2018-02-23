952ee5560b846be435ec5fe5a6afff79f389a9a1
Doug Cole <idugcoal@gmail.com>
2018-02-22 17:28:15 -0800

    adds handleUsername and handlePassword functions to Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,12 +4,31 @@ import styles from './Login.css';
 class Login extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
     this.onClick = this.onClick.bind(this);
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+  }
+
+  handleUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  handlePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
   }
 
   onClick(e) {
     e.preventDefault();
-    console.log('testing')
+    console.log(this.state.username, this.state.password)
   }
 
   render() {
@@ -17,8 +36,22 @@ class Login extends Component {
       <div className={styles.login}>
         <h2 className={styles.loginHeader}>Log in</h2>
         <form className={styles.loginContainer}>
-          <p><input type="email" placeholder="Email" autoComplete="on" /></p>
-          <p><input type="password" placeholder="Password" autoComplete="on" /></p>
+          <p>
+            <input 
+              type="email" 
+              placeholder="Username" 
+              autoComplete="on"
+              onChange={this.handleUsername} 
+            />
+          </p>
+          <p>
+            <input 
+            type="password" 
+            placeholder="Password" 
+            autoComplete="on" 
+            onChange={this.handlePassword}
+            />
+          </p>
           <p><button type="submit" onClick={this.onClick}>Log In</button></p>
         </form>
       </div>
